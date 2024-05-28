da54832a8f463c70484e8ddf91d56c71306c7e7f
Juliano Sartorelli <juliano.franco@datacake.com.br>
2024-05-28 20:08:51 -0300

    Finishing the mod, still need some clean up

--- Class1.cs
+++ Class1.cs
@@ -1,18 +1,67 @@
 ﻿using System;
+using Force.DeepCloner;
 using Microsoft.Xna.Framework;
+using Netcode;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.Locations;
+using StardewValley.Objects;
 
 namespace YourProjectName
 {
     internal sealed class ModEntry : Mod
     {
+        Dictionary<string, NetString> floors = new Dictionary<string, NetString>();
+        Dictionary<string, NetString> wallpapers = new Dictionary<string, NetString>();
+
+        private void Print(string e)
+        {
+            this.Monitor.Log(e, LogLevel.Debug);
+        }
+
         public override void Entry(IModHelper helper)
         {
             Helper.Events.GameLoop.SaveLoaded += this.OnLocationListChanged;
             Helper.Events.Player.Warped += this.OnLocationListChanged;
+            Helper.Events.Player.InventoryChanged += this.GetUsedDecorationAndRetrieve;
+        }
+
+        private void GetUsedDecorationAndRetrieve(object? sender, InventoryChangedEventArgs e)
+        {
+            if (!e.IsLocalPlayer) return;
+
+            DecoratableLocation location = (DecoratableLocation)Game1.player.currentLocation;
+            if (!location.GetType().IsSubclassOf(typeof(DecoratableLocation))) return;
+
+            List<Item> usedDecorations = new List<Item>();
+            foreach (Item item in e.Removed)
+            {
+                if (item.Name == "Wallpaper" || item.Name == "Flooring") usedDecorations.Add(item);
+            }
+
+            if (usedDecorations.Count < 1) return;
+
+            
+
+            foreach(Item item in usedDecorations)
+            {
+                if (item.Name == "Wallpaper")
+                {
+                    var difference = this.wallpapers.Except(location.appliedWallpaper.FieldDict);
+                    foreach (var item2 in difference) Game1.player.addItemToInventory(new Wallpaper(Int32.Parse(item2.Value), false));
+                    this.SaveWallpapers(location);
+
+                }
+
+                if (item.Name == "Flooring")
+                {
+                    var difference = this.floors.Except(location.appliedFloor.FieldDict);
+                    foreach (var item2 in difference) Game1.player.addItemToInventory(new Wallpaper(Int32.Parse(item2.Value), true));
+                    this.SaveFloors(location);
+                }
+            }
         }
 
 
@@ -22,8 +71,22 @@ namespace YourProjectName
             if (!Context.IsWorldReady)
                 return;
 
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.currentLocation.GetType()}", LogLevel.Debug);
+            var currentLocation = Game1.player.currentLocation;
+            var isDecoratable = currentLocation.GetType().IsSubclassOf(typeof(DecoratableLocation));
+            if (isDecoratable) {
+                this.SaveFloors((DecoratableLocation)currentLocation);
+                this.SaveWallpapers((DecoratableLocation)currentLocation);
+            }
+        }
+
+        private void SaveFloors(DecoratableLocation farmHouse)
+        {
+            this.floors = farmHouse.appliedFloor.FieldDict.DeepClone();
+        }
+
+        private void SaveWallpapers(DecoratableLocation farmHouse)
+        {
+            this.wallpapers = farmHouse.appliedWallpaper.FieldDict.DeepClone();
         }
     }
 }
